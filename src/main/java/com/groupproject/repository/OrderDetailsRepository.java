0eadbd64c633081e752facecdcd907315a358317
Michalis-Karvelas <mixalis_neila@hotmail.com>
2021-01-03 23:34:36 +0200

    +orderDetails

---
+++ src/main/java/com/groupproject/repository/OrderDetailsRepository.java
@@ -0,0 +1,7 @@
+package com.groupproject.repository;
+
+import com.groupproject.entities.OrderDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails, Long> {
+}
