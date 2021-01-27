ae27243b410ec129a0f3bc77c17aac4a98a5ce49
Giannis Katsanakis <giannis.katsanakis1@gmail.com>
2021-01-27 12:00:48 +0200

    plus annotations for tomcat

--- src/main/java/com/groupproject/security/WebConfig.java
+++ src/main/java/com/groupproject/security/WebConfig.java
@@ -1,10 +1,13 @@
 package com.groupproject.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableMBeanExport(registration= RegistrationPolicy.IGNORE_EXISTING)
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
